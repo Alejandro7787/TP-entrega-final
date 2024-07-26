cc22c4439bcc1fbd21d43e16232fd168fefbcf50
Alejandro7787 <167262111+Alejandro7787@users.noreply.github.com>
2024-07-26 12:34:51 +0000

    arreglado carrusel

--- src/Promociones.jsx
+++ src/Promociones.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import remera from './img/remera verde.jpg';
+import remera from './img/bosque.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Encabezado from './Encabezado';
 import Pie from './Pie';
+import './Promociones.css'
 
 
 function Promociones() {
@@ -11,7 +12,8 @@ function Promociones() {
     <div>
       <Encabezado />
     <div >
-      <Carousel className='flex-carrusel' data-bs-theme="dark">
+    <div className='grid-carrusel'>
+      <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
           className="d-block"
@@ -19,8 +21,8 @@ function Promociones() {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h5 className='texto-carrusel'>First slide label</h5>
+          <p className='texto-carrusel'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -30,8 +32,8 @@ function Promociones() {
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h5 className='texto-carrusel'>Second slide label</h5>
+          <p className='texto-carrusel'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
        <Carousel.Item>
@@ -41,8 +43,8 @@ function Promociones() {
             alt="Third slide"
           />
           <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
+            <h5 className='texto-carrusel'>Third slide label</h5>
+            <p className='texto-carrusel'>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Caption>
@@ -52,6 +54,7 @@ function Promociones() {
       <div className="flex-productos-cr">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta earum iste, alias eaque temporibus placeat saepe, dolores ipsam libero voluptates reiciendis dolorem quisquam repudiandae sint animi. Ab voluptas officiis magnam.</p>
         <button className="boton">Lo quiero!!</button>
+      </div>
       </div>
     </div>
     <Pie />
