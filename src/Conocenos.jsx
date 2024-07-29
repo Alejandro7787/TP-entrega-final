347e02ab7eafa9b1158b99cd6aa53374b88ecf07
Alejandro7787 <almiron.alejandro87@gmail.com>
2024-07-28 23:06:48 -0300

    nuevas alternativas

--- src/Conocenos.jsx
+++ src/Conocenos.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Encabezado from './Encabezado'
 import Pie from './Pie';
+import Nosotros from './img/Nosotros.jpg';
 
 function Conocenos() {
   return (
     <div>
       <Encabezado/>
-        <div>
-          
+        <div className='grid-nosotros'>
+          <img src={Nosotros} alt='Nosotros'/>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum architecto dolorum quas aliquam voluptas inventore reiciendis exercitationem quia quam, amet blanditiis optio cumque impedit atque nobis obcaecati. Ipsam, hic iure?</p>
         </div>
       <Pie />
     </div>
